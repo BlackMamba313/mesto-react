1927bca2630459ee090bf3c561cf3dd8cab8b4f0
Alexey Konstantinov <applejack66@yandex.ru>
2021-07-06 16:38:44 +0300

    after avtotest

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,5 +1,5 @@
 import { useContext  } from 'react';
-import Card from "./Сard";
+import Card from "./Card";
 import { CurrentUserContext } from "../contexts/CurrentUserContext";
 
 function Main(props) {
